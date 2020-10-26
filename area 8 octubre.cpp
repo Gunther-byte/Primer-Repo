64c5d10da5e5e8b5872b083849507940fbd6a75c
Gunther-byte <72039620+Gunther-byte@users.noreply.github.com>
2020-10-26 02:10:16 -0600

    Create area 8 octubre.cpp

---
+++ area 8 octubre.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h> //Input Output
+#include<math.h>
+
+#define PI 3.1416 //Definiendo una constante
+
+int main(){
+	float area, radio; // Declarando variables de tipo flotante
+	radio = 5; //Asignando valor a variable radio
+	area = PI * pow(5,2); //Asignando resultado de la operacion variable
+	printf("Area\n"); //Imprimiendo Titulo
+	// Imprimiendo resultado
+	printf("Area de Circulo con radio 5: %f", area);
+}
